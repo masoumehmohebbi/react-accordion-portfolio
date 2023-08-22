646d8850149c9dfe51308801fe72b23fe8c3bc3c
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-22 21:56:17 +0330

    Added Group-class SideAccordion.jsx

--- src/components/SideAccordion.jsx
+++ src/components/SideAccordion.jsx
@@ -1,12 +1,14 @@
 function SideAccordion() {
   return (
-    <aside className="col-span-3 h-full flex justify-end">
-      <div className="flex flex-col items-center border-l-[1px] border-primary justify-between h-full w-[70px] py-11">
-        <h1>01</h1>
-        <h1 className=" rotate-90 ">About</h1>
+    <aside className="font-Montserrat z-50 col-span-3 h-full flex justify-end">
+      <div className="group flex flex-col items-center border-l-[1px] border-primary justify-between h-full w-[70px] py-11">
+        <h1 className="group-hover:scale-[3.5] group-hover:font-jetbrains duration-150 ">
+          01
+        </h1>
+        <h1 className="rotate-90">About</h1>
       </div>
       <div className="flex flex-col items-center border-l-[1px] border-primary justify-between h-full w-[70px] py-11">
-        <h1>02</h1>
+        <h1 className="font-mono">02</h1>
         <h1 className=" rotate-90 ">About</h1>
       </div>
       <div className="flex flex-col items-center border-l-[1px] border-primary justify-between h-full w-[70px] py-11">
