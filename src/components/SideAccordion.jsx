a7d1884bcf22fa08cb952912d52887affc04bfb4
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-24 12:28:06 +0330

    Added accordionBanner SideAccordion.jsx

--- src/components/SideAccordion.jsx
+++ src/components/SideAccordion.jsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 function SideAccordion() {
   const [accordionBanner] = useState([
     { id: "01", title: "About" },
-    // { id: "02", title: "Portfolio" },
-    // { id: "03", title: "CV" },
-    // { id: "04", title: "Contact" },
+    { id: "02", title: "Portfolio" },
+    { id: "03", title: "CV" },
+    { id: "04", title: "Contact" },
   ]);
   return (
-    <aside className="z-50 col-span-3 h-full flex justify-end ">
+    <aside className="z-50 h-full flex absolute right-0">
       {accordionBanner.map((banner) => (
-        <Accordion key={banner.id} banner={banner} />
+        <AccordionItem key={banner.id} banner={banner} />
       ))}
     </aside>
   );
@@ -18,9 +18,15 @@ function SideAccordion() {
 
 export default SideAccordion;
 
-function Accordion({ banner }) {
+function AccordionItem({ banner }) {
+  // bg-red-500 w-[95%] absolute right-0
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="group" key={banner.id}>
+    <div
+      className={`group flex ${isOpen ? " bg-red-500 w-[67.8rem]" : ""}`}
+      key={banner.id}
+      onClick={() => setIsOpen((pervIs) => !pervIs)}
+    >
       <h1 className="group-hover:-ml-12 group-hover:text-lg group-hover:scale-[3.3] group-hover:font-Londrina">
         {banner.id}
       </h1>
