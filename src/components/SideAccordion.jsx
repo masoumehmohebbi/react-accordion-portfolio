574d08653b38cce0df79aaa993cfe2e06ee19d0f
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-24 09:37:03 +0330

    Added SideAccordion.jsx

--- src/components/SideAccordion.jsx
+++ src/components/SideAccordion.jsx
@@ -1,26 +1,30 @@
+import { useState } from "react";
+
 function SideAccordion() {
+  const [accordionBanner] = useState([
+    { id: "01", title: "About" },
+    // { id: "02", title: "Portfolio" },
+    // { id: "03", title: "CV" },
+    // { id: "04", title: "Contact" },
+  ]);
   return (
-    <aside className="font-Montserrat z-50 col-span-3 h-full flex justify-end">
-      <div className="group flex flex-col items-center border-l-[1px] border-primary justify-between h-full w-[70px] py-11">
-        <h1 className="group-hover:scale-[3.5] group-hover:font-jetbrains duration-150 ">
-          01
-        </h1>
-        <h1 className="rotate-90">About</h1>
-      </div>
-      <div className="flex flex-col items-center border-l-[1px] border-primary justify-between h-full w-[70px] py-11">
-        <h1 className="font-mono">02</h1>
-        <h1 className=" rotate-90 ">About</h1>
-      </div>
-      <div className="flex flex-col items-center border-l-[1px] border-primary justify-between h-full w-[70px] py-11">
-        <h1>03</h1>
-        <h1 className=" rotate-90 ">About</h1>
-      </div>
-      <div className="flex flex-col items-center border-l-[1px] border-primary justify-between h-full w-[70px] py-11">
-        <h1>04</h1>
-        <h1 className=" rotate-90 ">About</h1>
-      </div>
+    <aside className="z-50 col-span-3 h-full flex justify-end ">
+      {accordionBanner.map((banner) => (
+        <Accordion key={banner.id} banner={banner} />
+      ))}
     </aside>
   );
 }
 
 export default SideAccordion;
+
+function Accordion({ banner }) {
+  return (
+    <div className="group" key={banner.id}>
+      <h1 className="group-hover:-ml-12 group-hover:text-lg group-hover:scale-[3.3] group-hover:font-Londrina">
+        {banner.id}
+      </h1>
+      <h2 className="rotate-90">{banner.title}</h2>
+    </div>
+  );
+}
