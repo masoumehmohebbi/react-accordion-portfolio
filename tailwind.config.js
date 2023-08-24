fedda8543a19499fd1b54813677c43a7eee8f5f8
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-24 09:34:24 +0330

    Added Custom fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         primary: "#222",
+        secondary: "#f7f6f1",
       },
       fontFamily: {
-        paceMono: ["Space Mono", "monospace"],
-        Montserrat: ["Montserrat", "sans-serif"],
         jetbrains: ["JetBrains Mono", "monospace"],
+        Londrina: ["Londrina Shadow", "cursive"],
+        rubik: ["Nosifer", "cursive"],
       },
     },
   },
