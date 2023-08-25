771036288d0e0743583ff583eeddc35c2b2fd6f9
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-25 17:10:24 +0330

    Added GoogleMap Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,22 @@
+import {
+  BiMailSend,
+  BiMap,
+  BiPhoneCall,
+  BiLogoInstagram,
+  BiLogoLinkedin,
+  BiLogoGithub,
+  BiLogoTelegram,
+} from "react-icons/bi";
+
 function Contact() {
   return (
-    <section>
+    <section className="font-jetbrains">
+      <Map />
       <div className="flex w-[64.9rem] gap-x-16 p-11">
-        <HandleMessages />
+        <Messages />
         <Details />
       </div>
+      <Footer />
     </section>
   );
 }
@@ -12,37 +24,33 @@ function Contact() {
 export default Contact;
 
 // Messages Form CMP
-function HandleMessages() {
-  return (
-    <div className="flex flex-1 flex-col">
-      <h1>Message Me</h1>
-      <form action="#" className="gap-y-8 flex flex-col">
-        <input
-          placeholder="Name"
-          className=" border-b-[1px] border-primary bg-transparent "
-          type="text"
-        />
-        <input
-          placeholder="Email"
-          className=" border-b-[1px] border-primary bg-transparent "
-          type="text"
-        />
-        <input
-          placeholder="Subject"
-          className=" border-b-[1px] border-primary bg-transparent "
-          type="text"
-        />
-        <textarea
-          placeholder="Message"
-          className=" border-b-[1px] border-primary  bg-transparent"
-          rows={4}
-        />
+function Messages() {
+  const HandleSubmitForm = (e) => {
+    e.preventDefault();
+  };
 
-        <input
+  return (
+    <div className="flex flex-1 flex-col text-primary dark:text-secondary">
+      <h1 className=" text-2xl font-bold pb-9">
+        <span className="underline underline-offset-8">Me</span>
+        ssage Me
+      </h1>
+      <form
+        onSubmit={HandleSubmitForm}
+        action="#"
+        className="gap-y-8 flex flex-col"
+      >
+        <input placeholder="Name" type="text" />
+        <input placeholder="Email" type="email" />
+        <input placeholder="Subject" type="text" />
+        <textarea placeholder="Message" rows={4} />
+        <button
           type="submit"
-          value="send mail"
-          className=" rounded-3xl bg-primary text-secondary p-2 w-32 uppercase text-xs"
-        />
+          className="flex items-center justify-evenly border-[1px] hover:border-primary dark:hover:border-secondary hover:bg-transparent hover:text-primary dark:hover:text-secondary  dark:hover:bg-transparent rounded-3xl bg-primary dark:bg-secondary dark:text-primary text-secondary p-2 w-32 uppercase text-xs duration-500"
+        >
+          send mail
+          <BiMailSend className="text-lg" />
+        </button>
       </form>
     </div>
   );
@@ -51,14 +59,70 @@ function HandleMessages() {
 // Details CMP
 function Details() {
   return (
-    <div className="flex flex-1 flex-col">
-      <h1>My Details</h1>
+    <div className="flex flex-1 flex-col text-primary dark:text-secondary">
+      <h1 className="  text-2xl font-bold pb-9">
+        <span className="underline underline-offset-8">My</span> Details
+      </h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto,
         consequatur quam? Nemo amet ad quas nam illo suscipit aspernatur
         eligendi exercitationem nihil quod corporis, deserunt aperiam
         consequuntur nulla velit ipsum.
       </p>
+      <div className="flex flex-col gap-y-4 mt-8">
+        <span className="flex gap-x-2 font-semibold text-sm">
+          <BiMailSend className=" p-1 bg-primary dark:bg-secondary text-3xl text-secondary dark:text-primary" />
+          Masoumeh.mohebbi@gmail.com
+        </span>
+        <span className="flex gap-x-2 font-semibold text-sm">
+          <BiPhoneCall className=" p-1 bg-primary dark:bg-secondary text-3xl text-secondary dark:text-primary" />
+          09394296023
+        </span>
+        <span className="flex gap-x-2 font-semibold text-sm">
+          <BiMap className=" p-1 bg-primary dark:bg-secondary text-3xl text-secondary dark:text-primary" />
+          IR, Guilan, Rasht
+        </span>
+      </div>
     </div>
+  );
+}
+
+// Footer
+function Footer() {
+  return (
+    <footer className="mt-4">
+      <div className="h-[1px] w-11/12 bg-primary dark:bg-secondary mx-auto"></div>
+
+      <div className="flex flex-col text-primary dark:text-secondary gap-y-6 w-full items-end mt-6 p-5">
+        <h5>© 2023 Masoumeh Mohebbi | Portfolio.</h5>
+        <span className="flex gap-x-5 text-2xl ">
+          <BiLogoInstagram />
+          <BiLogoTelegram />
+          <BiLogoLinkedin />
+          <BiLogoGithub />
+        </span>
+      </div>
+    </footer>
+  );
+}
+
+// Map
+function Map() {
+  return (
+    <section className="mt-16 text-primary dark:text-secondary">
+      <h1 className=" font-Montserrat px-11 py-4 text-3xl font-bold">
+        {" "}
+        GET IN TOUCH
+      </h1>
+      <div className="h-[1px] w-11/12 bg-primary dark:bg-secondary mx-auto"></div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6349.957596401076!2d49.59718440074156!3d37.27192985743835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ff5620d43f15675%3A0x41dd40f9931cfbf6!2z2LHYtNiq2Iwg2KfYs9iq2KfZhiDar9uM2YTYp9mG2Iwg2KfbjNix2KfZhg!5e0!3m2!1sfa!2snl!4v1692967977726!5m2!1sfa!2snl"
+        width="950"
+        height="300"
+        allowfullscreen=""
+        loading="lazy"
+        className="border border-primary m-11"
+      ></iframe>
+    </section>
   );
 }
