7bfdc22a1a2b19b5172535238900f981d49a62ae
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-26 09:50:07 +0330

    Added Slider About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,16 @@
+import Slider from "./Slider";
+
 function About() {
-  return <div>About</div>;
+  return (
+    <section>
+      <h1 className=" font-Montserrat px-11 py-4 text-3xl font-bold uppercase">
+        {" "}
+        About me
+      </h1>
+      <div className="h-[1px] w-11/12 bg-primary dark:bg-secondary mx-auto"></div>
+      <Slider />
+    </section>
+  );
 }
 
 export default About;
