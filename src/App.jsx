3ab98f32f2ff9143bf26d73ddf2d4fb23bc44837
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-28 18:24:24 +0330

    Seprated Content-CMP App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,28 @@ import SideNav from "./components/SideNav";
 import { Typewriter } from "react-simple-typewriter";
 import SideAccordion from "./components/SideAccordion";
 import { motion } from "framer-motion";
+import About from "./components/About";
+
 export default function App() {
+  return (
+    <div className="dark:bg-primary cursor-none relative w-full h-full">
+      <section className="flex h-screen justify-between w-full pt-5 lg:pt-0">
+        <SideNav />
+
+        <Content />
+
+        <SideAccordion />
+      </section>
+      <section className="+md:hidden">
+        <About />
+      </section>
+    </div>
+  );
+}
+
+function Content() {
   const magic = useRef(null);
+  const [cursorVarient, setCursorVarient] = useState("default");
 
   useEffect(() => {
     const magicWHalf = magic.current.offsetWidth / 2;
@@ -17,9 +37,6 @@ export default function App() {
       document.body.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
-
-  const [cursorVarient, setCursorVarient] = useState("default");
-
   const variants = {
     default: {
       zIndex: 5,
@@ -33,53 +50,42 @@ export default function App() {
   const textLeave = () => setCursorVarient("default");
 
   return (
-    <div className="dark:bg-primary cursor-none relative h-screen w-screen ">
-      <section className="flex h-full justify-between w-full">
-        <SideNav />
-
-        <main
-          onMouseEnter={textEnter}
-          onMouseLeave={textLeave}
-          className="h-full p-20 w-full col-span-8 overflow-hidden flex flex-col gap-y-8 items-start justify-end"
-        >
-          <h1 className="font-Montserrat text-primary dark:text-secondary text-5xl lg:text-7xl font-black  uppercase font-test relative">
-            IM MASOUME <br /> MOHEBBI.
-          </h1>
-          <div className="font-rubik text-[14px] text-primary dark:text-secondary">
-            Im a freelance{" "}
-            <span className=" bg-[#f7bf00] p-2">
-              <Typewriter
-                words={[
-                  "front end engineer.",
-                  "web developer.",
-                  "coffee drinker.",
-                ]}
-                loop={0}
-                typeSpeed={120}
-                deleteSpeed={80}
-                delaySpeed={1000}
-                cursorStyle="|"
-                cursor
-              />
-            </span>
-            <br />
-            From IR.
-          </div>
-          {/* <div
-          ref={magicRef}
-          className="magic bg-[url('/src/assets/images/background.jpg')] absolute w-40 h-40  bg-center bg-no-repeat bg-fixed rounded-full z-5"
-        ></div> */}
-        </main>
-
-        <SideAccordion />
-      </section>
-
+    <>
+      <main
+        onMouseEnter={textEnter}
+        onMouseLeave={textLeave}
+        className="h-full p-20 w-[calc(100%-65px)] col-span-8 overflow-hidden flex flex-col gap-y-8 items-start justify-end"
+      >
+        <h1 className="font-Montserrat text-primary dark:text-secondary text-5xl lg:text-7xl font-black  uppercase font-test relative">
+          IM MASOUME <br /> MOHEBBI.
+        </h1>
+        <div className="font-rubik text-[14px] text-primary dark:text-secondary">
+          Im a freelance{" "}
+          <span className=" bg-[#f7bf00] p-2">
+            <Typewriter
+              words={[
+                "front end engineer.",
+                "web developer.",
+                "coffee drinker.",
+              ]}
+              loop={0}
+              typeSpeed={120}
+              deleteSpeed={80}
+              delaySpeed={1000}
+              cursorStyle="|"
+              cursor
+            />
+          </span>
+          <br />
+          From IR.
+        </div>
+      </main>
       <motion.div
         variants={variants}
         animate={cursorVarient}
         className="cursor -z-20 dark:z-[5] absolute w-4 h-4 bg-[url('/src/assets/images/background.jpg')] bg-[50%_50%] bg-fixed rounded-full bg-no-repeat bg-cover"
         ref={magic}
       ></motion.div>
-    </div>
+    </>
   );
 }
