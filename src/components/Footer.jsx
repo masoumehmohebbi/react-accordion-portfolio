4545af8fff14f80dc3b4cbfaf3a2243f256ca7b1
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-28 09:20:58 +0330

    Added Dvider Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ import {
 export default function Footer() {
   return (
     <footer className="mt-4">
-      <div className="h-[1px] w-11/12 bg-primary dark:bg-secondary mx-auto"></div>
+      <div className="h-[1px] w-11/12 bg-primary dark:bg-secondary mx-auto mt-9"></div>
 
       <div className="flex flex-col text-primary dark:text-secondary gap-y-6 w-full items-end mt-6 p-5">
         <h5>© 2023 Masoumeh Mohebbi | Portfolio.</h5>
