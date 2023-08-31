6f100fbb0acd845618869e836b4c53e4188966de
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-31 19:56:12 +0330

    Updated Styles SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -8,27 +8,27 @@ import DarkModeToggle from "./DarkModeToggle";
 
 function SideNav() {
   return (
-    <nav className="z-50 fixed top-0 cursor-default h-full flex flex-col justify-between w-[72px] border-r-[1px] border-primary dark:border-secondary">
+    <nav className="z-50 fixed top-0 cursor-default h-full flex flex-col justify-between w-[50px] +md:w-[72px] border-r-[1px] border-primary dark:border-secondary">
       <div className="flex items-center justify-center ">
-        <BiHomeHeart className="w-11 h-11 mt-1 text-primary dark:text-secondary" />
+        <BiHomeHeart className="w-9 h-9 +md:w-11 +md:h-11 mt-1 text-primary dark:text-secondary" />
       </div>
       <ul>
         <li>
-          <DarkModeToggle />
+          <DarkModeToggle className="w-8 h-8 +md:w-9 +md:h-9" />
         </li>
         <li>
           <a href="#">
-            <BiLogoInstagram className="w-9 h-9" />
+            <BiLogoInstagram className="w-8 h-8 +md:w-9 +md:h-9" />
           </a>
         </li>
         <li>
           <a href="#">
-            <BiLogoGithub className="w-9 h-9" />
+            <BiLogoGithub className="w-8 h-8 +md:w-9 +md:h-9" />
           </a>
         </li>
         <li>
           <a href="#">
-            <BiLogoLinkedin className="w-9 h-9" />
+            <BiLogoLinkedin className="w-8 h-8 +md:w-9 +md:h-9" />
           </a>
         </li>
       </ul>
