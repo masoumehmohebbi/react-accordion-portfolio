2aad54600efd4e0af39f59a2ef513f173b3d297f
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-08-31 19:55:26 +0330

    Updated Styles Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
     <footer className="mt-4">
       <div className="h-[1px] w-11/12 bg-primary dark:bg-secondary mx-auto mt-9"></div>
 
-      <div className="flex flex-col text-primary dark:text-secondary gap-y-6 w-full items-end mt-6 p-5">
+      <div className="flex flex-col text-primary dark:text-secondary gap-y-6 w-full items-end mt-6 text-sm +md:text-base py-5 pr-5 +md:p-5">
         <h5>© 2023 Masoumeh Mohebbi | Portfolio.</h5>
         <span className="flex gap-x-5 text-2xl ">
           <BiLogoInstagram />
