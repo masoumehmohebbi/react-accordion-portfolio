1c6eeadb52d2e75a23d5d932af6cf3fee9452ff2
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-02 11:14:24 +0330

    Dynamic Mobile-Nav SideNav.jsx

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -3,37 +3,111 @@ import {
   BiLogoLinkedin,
   BiLogoGithub,
   BiHomeHeart,
+  BiX,
+  BiMenu,
 } from "react-icons/bi";
 import DarkModeToggle from "./DarkModeToggle";
 
-function SideNav() {
+function SideNav({ openMobileNav, setOpenMobileNav }) {
   return (
-    <nav className="z-50 fixed +md:static top-0 cursor-default h-full flex flex-col justify-between w-[50px] +md:w-[72px] border-r-[1px] border-primary dark:border-secondary">
-      <div className="flex items-center justify-center ">
-        <BiHomeHeart className="w-9 h-9 +md:w-11 +md:h-11 mt-1 text-primary dark:text-secondary" />
-      </div>
-      <ul>
-        <li>
-          <DarkModeToggle className="w-8 h-8 +md:w-9 +md:h-9" />
-        </li>
-        <li>
-          <a href="#">
-            <BiLogoInstagram className="w-8 h-8 +md:w-9 +md:h-9" />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <BiLogoGithub className="w-8 h-8 +md:w-9 +md:h-9" />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <BiLogoLinkedin className="w-8 h-8 +md:w-9 +md:h-9" />
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      {openMobileNav ? <MobileSideNav /> : ""}
+      <nav
+        className={`z-50 ${
+          openMobileNav ? "ml-[117px]" : "ml-0"
+        }  fixed +md:static top-0 cursor-default h-full flex flex-col justify-between w-[50px] +md:w-[72px] border-r-[1px] border-primary dark:border-secondary`}
+      >
+        <a href="#home" className="flex items-center justify-center z-50">
+          <BiHomeHeart className="hidden +md:block +md:w-11 +md:h-11 mt-1 text-primary dark:text-secondary" />
+
+          <span
+            className="+md:hidden"
+            onClick={() => setOpenMobileNav(!openMobileNav)}
+          >
+            {openMobileNav ? (
+              <BiX className="w-9 h-9 text-primary dark:text-secondary mt-1" />
+            ) : (
+              <BiMenu className="w-9 h-9 text-primary dark:text-secondary mt-1" />
+            )}
+          </span>
+        </a>
+
+        <ul>
+          <li>
+            <DarkModeToggle className="w-8 h-8 +md:w-9 +md:h-9" />
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://instagram.com/masoume.frontend.js"
+            >
+              <BiLogoInstagram className="w-8 h-8 +md:w-9 +md:h-9" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/masoumehmohebbi"
+            >
+              <BiLogoGithub className="w-8 h-8 +md:w-9 +md:h-9" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/masoumemohebbi"
+            >
+              <BiLogoLinkedin className="w-8 h-8 +md:w-9 +md:h-9" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
 export default SideNav;
+
+function MobileSideNav() {
+  return (
+    <div className="z-50 font-jetbrains transition-all duration-500 bg-primary dark:bg-secondary fixed +md:hidden top-0 cursor-default h-full flex flex-col justify-between border-r-[1px] border-primary dark:border-secondary">
+      <ul>
+        <li className="border-b border-b-[#333] py-5 pl-2 pr-11 uppercase">
+          <a
+            href="#home"
+            className=" text-[#ffffffcc] dark:text-primary text-xs"
+          >
+            Home
+          </a>
+        </li>
+        <li className="border-b border-b-[#333] py-5 pl-2 pr-11 uppercase">
+          <a
+            href="#about"
+            className=" text-[#ffffffcc] dark:text-primary text-xs"
+          >
+            About
+          </a>
+        </li>
+        <li className="border-b border-b-[#333]  py-5 pl-2 pr-11 uppercase">
+          <a
+            href="#portfolio"
+            className=" text-[#ffffffcc] dark:text-primary text-xs"
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="border-b border-b-[#333] py-5 pl-2 pr-11 uppercase">
+          <a
+            href="#contact"
+            className=" text-[#ffffffcc] dark:text-primary text-xs"
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
